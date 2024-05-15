e51d16629f39ad7691c88cfe4e4468c244c48032
Marco Maluco <159474548+Markyneos@users.noreply.github.com>
2024-05-15 12:55:51 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ function converter_Temperatura(){
     if (isNaN(temperatura_celsius)){
         alert("Digite um número válido!");
     }
+    else if (temperatura_celsius < -273.15) {
+        alert("A temperatura não pode ser inferior ao zero absoluto (-273.15 °C)!");
+    }
     else{
         var temperaturaFahrenheit = (1.8*temperatura_celsius) + 32;
         resultadoElement = document.getElementById("resultado");
     resultadoElement.innerHTML = "A temperatura é igual a: " + temperaturaFahrenheit.toFixed(2) + "° Fahrenheit";
     }
 }
-function limparConsole(){
+function limparConsole() {
     resultadoElement.innerHTML = " ";
 }
