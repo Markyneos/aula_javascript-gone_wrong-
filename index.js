0ed4ed99327645343910ba1c8a7faf9f2d152981
Marco Maluco <159474548+Markyneos@users.noreply.github.com>
2024-05-15 10:40:48 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+var resultadoElement;
 function converter_Temperatura(){
-    var temperatura_celsius = prompt("Digite a temperatura em Celsius:");
+    var temperatura_celsius = parseFloat(document.getElementById('temperatura').value);
     if (isNaN(temperatura_celsius)){
         alert("Digite um número válido!");
     }
     else{
-        var temperatura_fahrenheit = 1.8*temperatura_celsius + 32;
-    alert("Temperatura em fahrenheit: " + temperatura_fahrenheit);
+        var temperaturaFahrenheit = (1.8*temperatura_celsius) + 32;
+        resultadoElement = document.getElementById("resultado");
+    resultadoElement.innerHTML = "A temperatura é igual a: " + temperaturaFahrenheit.toFixed(2) + "° Fahrenheit";
     }
+}
+function limparConsole(){
+    resultadoElement.innerHTML = " ";
 }
